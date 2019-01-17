0fb8d960b1cc5871dc010e11fae530f4e88acf5f
Raymond Gregory <vinedsel@gmail.com>
2019-01-17 14:38:22 -0600

    Added project code.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+class List extends React.Component {
+    render() {
+        console.log("List's render function"); // this should not be logged multiple times if the exact same props are provided a second time
+      const list = this.props.items.map(item => (<li key={item}>{item}</li>));
+
+      return (
+        <ul>
+            {list}
+        </ul>
+      );
+    }
+  }
+
+  const list1Items = ['Eggs', 'Bread', 'Artisinal cheese'];
+  const list2Items = ['Trains', 'Planes', 'Automobiles'];
+
+  const renderItems = (items) => {
+    render(
+      <List items={items} />,
+      document.getElementById('root')
+    );
+  }
+
+  document.addEventListener('keydown', event => {
+    // this checks if the 1 key is pressed
+    if (event.code === 'Digit1') {
+        renderItems(list1Items);
+    }
+    // this checks if the 2 key is pressed
+    else if (event.code === 'Digit2') {
+        renderItems(list2Items);
+    }
+  });
+
+  renderItems(list1Items);
